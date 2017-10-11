714d726382bae5d67836da0d03183a09dabb7238
Shaun Walker <shaun.walker@arrowdigital.com>
2017-10-10 23:14:19 -0400

    Updated script to add the host setting for control panel if it does not exist already ( this occurs during CMS installation as this module is installed before the DNN install template is processed )

--- DesktopModules/HCC/Application/Scripts/03.00.00.sql
+++ DesktopModules/HCC/Application/Scripts/03.00.00.sql
@@ -1,4 +1,12 @@
-UPDATE {databaseOwner}{objectQualifier}HostSettings
-	SET SettingValue='DesktopModules/Hotcakes/ControlPanel/ControlBar.ascx'
-	WHERE SettingName='ControlPanel'
+IF NOT EXISTS ( SELECT 1 FROM {databaseOwner}{objectQualifier}HostSettings WHERE SettingName = 'ControlPanel' )
+BEGIN
+  INSERT INTO {databaseOwner}{objectQualifier}HostSettings ( SettingName, SettingValue, SettingIsSecure, CreatedByUserID, CreatedOnDate, LastModifiedByUserID, LastModifiedOnDate )
+  VALUES ( 'ControlPanel', 'DesktopModules/Hotcakes/ControlPanel/ControlBar.ascx', 0, -1, getdate(), -1, getdate() )
+END
+ELSE
+BEGIN
+  UPDATE {databaseOwner}{objectQualifier}HostSettings
+	SET SettingValue = 'DesktopModules/Hotcakes/ControlPanel/ControlBar.ascx'
+	WHERE SettingName = 'ControlPanel'
+END
 GO
